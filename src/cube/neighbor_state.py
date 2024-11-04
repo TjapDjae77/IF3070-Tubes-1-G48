bfce9414d28bbde7a70fa5ce33b95e4784e175d1
muhammadkasyfil <141574422+muhammadkasyfil@users.noreply.github.com>
2024-11-04 13:48:41 +0700

    simulated annealing branch

---
+++ src/cube/neighbor_state.py
@@ -0,0 +1,14 @@
+import numpy as np
+import random
+
+class NeighborState:
+    # def __init__(self, neighbor_state):
+    #     self.neighbor_state = neighbor_state
+    #     self.size = magic_cube.size
+    #     self.magic_number = magic_cube.magic_number
+
+    def neighbor():
+#   menetapkan neighbor
+    
+    def swap_neighbor():
+#   swapping neighbor
