5bf7313e1d343491aca3900de18d95d9f7a67e37
TjapDjae77 <r.f.rayhan@gmail.com>
2024-11-05 18:20:32 +0700

    Benerin Objective Function

--- src/cube/objective_function.py
+++ src/cube/objective_function.py
@@ -20,14 +20,27 @@ class ObjectiveFunction:
     
                 # Jumlah error di tiang tiap layer
                 total_error += abs(np.sum(cube[:, i, j]) - self.magic_number)
-    
 
-        # Jumlah error di diagonal bidang tiap layer
+        # Diagonal bidang pada layer horizontal
         for i in range(self.size):
-            total_error += abs(np.sum(np.diag(cube[i])) - self.magic_number)
+            horizontal_diag_1 = np.sum(np.diag(cube[i]))
+            horizontal_diag_2 = np.sum(np.diag(np.fliplr(cube[i])))
+            total_error += abs(horizontal_diag_1 - self.magic_number)
+            total_error += abs(horizontal_diag_2 - self.magic_number)
 
-            total_error += abs(np.sum(np.diag(np.fliplr(cube[i]))) - self.magic_number)
+        # Diagonal menyilang horizontal dari depan ke belakang di tiap baris
+        for j in range(self.size):
+            front_back_diag_1 = np.sum([cube[i, j, i] for i in range(self.size)])
+            front_back_diag_2 = np.sum([cube[i, j, self.size - 1 - i] for i in range(self.size)])
+            total_error += abs(front_back_diag_1 - self.magic_number)
+            total_error += abs(front_back_diag_2 - self.magic_number)
 
+        # Diagonal menyilang vertikal dari sisi kiri ke kanan di tiap kolom
+        for k in range(self.size):
+            left_right_diag_1 = np.sum([cube[i, i, k] for i in range(self.size)])
+            left_right_diag_2 = np.sum([cube[i, self.size - 1 - i, k] for i in range(self.size)])
+            total_error += abs(left_right_diag_1 - self.magic_number)
+            total_error += abs(left_right_diag_2 - self.magic_number)
 
         # Jumlah error diagonal ruang kubus
         total_error += abs(np.sum([cube[i, i, i] for i in range(self.size)]) - self.magic_number)
